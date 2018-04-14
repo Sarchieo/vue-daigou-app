4efb38e266ff62d731bd2ebbeb20874d9fbf0823
Qi Liu <Hxbnzy@gmail.com>
2018-04-14 18:44:57 +0800

    底部导航栏添加

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import YDUI from 'vue-ydui' /* 相当于import YDUI from 'vue-ydui/ydui.rem.js' */
+import 'vue-ydui/dist/ydui.rem.css'
 
 Vue.config.productionTip = false
-
+Vue.use(YDUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/views/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <yd-layout>
+    <!--<yd-navbar slot="navbar" title="首页"></yd-navbar>-->
+    <yd-tabbar slot="tabbar">
+      <yd-tabbar-item title="首页" link="#" active>
+        <yd-icon name="home" slot="icon"></yd-icon>
+      </yd-tabbar-item>
+      <yd-tabbar-item title="购物车" link="#">
+        <yd-icon name="shopcart-outline" slot="icon"></yd-icon>
+      </yd-tabbar-item>
+      <yd-tabbar-item title="个人中心" link="#">
+        <yd-icon name="ucenter-outline" slot="icon"></yd-icon>
+      </yd-tabbar-item>
+    </yd-tabbar>
+  </yd-layout>
+</template>
+<script>
+export default{
+
+}
+</script>
+<style>
+
+</style>
