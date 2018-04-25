654090be3740823d58e784733994647eeb95e878
Sarchieo <hxbnzy@gmail.com>
2018-04-25 22:59:30 +0800

    ~~~

--- src/views/home/children/android.vue
+++ src/views/home/children/android.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <div  v-for="(item, index) in data" :key="index">
-      <mt-cell title="item.title" icon="more" value="带 icon"></mt-cell>
-      <!--<mt-cell title="标题文字" icon="more" value="带 icon"></mt-cell>-->
-      <!--<mt-cell title="标题文字" icon="more" value="带 icon"></mt-cell>-->
+  <div class="container">
+    <div v-for="(item, index) in newList" :key="index">
+      <!--<ul class="flex-container">{{item.title}}</ul>-->
+      <div class="flex-container">
+        <img class="avatar" :src="item.user.avatar_url"/>
+        <div>
+          <ul>{{item.title}}</ul>
+          <ul>{{item.node_name}}</ul>
+        </div>
+      </div>
     </div>
   </div>
 
@@ -13,7 +18,8 @@ import http from '../../../http'
 export default{
   data () {
     return {
-      data: []
+      newList: [],
+      node_id: 1
     }
   },
   methods: {
@@ -30,7 +36,8 @@ export default{
               timeout: 3000
             })
           }
-          console.log(response)
+          this.newList = response.data
+          console.log(this.newList)
         })
         .catch(err => {
           console.log(err)
@@ -47,5 +54,17 @@ export default{
     width:100%;
     height: 100%;
     background-color: gainsboro;
+  }
+  .flex-container {
+    margin-bottom: .1rem;
+    background-color: white;
+    display: flex;
+    height: 2rem;
+  }
+  .avatar {
+    margin: .2rem;
+    width: 1rem;
+    line-height: 2rem;
+    height: 1rem;
   }
 </style>
